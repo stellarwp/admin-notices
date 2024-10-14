41509359182872a1a6170d83d7e23913625b3216
Jason Adams <jason.adams@givewp.com>
2024-10-14 12:35:02 -0600

    fix: switches to filemtime for versioning, as strauss removes composer.json

--- src/AdminNotices.php
+++ src/AdminNotices.php
@@ -194,13 +194,12 @@ class AdminNotices
     /**
      * Hook action to enqueue the scripts needed for dismissing notices
      *
+     * @since 1.0.2 use filetime for versioning, which will bust the cache when the library is updated
      * @since 1.0.0
      */
     public static function enqueueScripts(): void
     {
-        // use the version from the composer.json file
-        $composerJson = json_decode(file_get_contents(__DIR__ . '/../composer.json'), true);
-        $version = $composerJson['version'];
+        $version = filemtime(__DIR__ . '/resources/admin-notices.js');
 
         wp_enqueue_script(
             'stellarwp-admin-notices',
